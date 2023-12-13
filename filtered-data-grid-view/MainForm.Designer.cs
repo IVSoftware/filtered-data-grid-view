58a994f57890bf22dc071bd8f68284353b5ebd49
Tom Gregor <tgregor@ivsoftware.com>
2023-12-13 07:47:09 -0700

    Add project files.

---
+++ filtered-data-grid-view/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace filtered_data_grid_view
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            dataGridView = new DataGridView();
+            checkBoxFilterChampions = new CheckBox();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 1539F));
+            tableLayoutPanel1.Controls.Add(dataGridView, 0, 1);
+            tableLayoutPanel1.Controls.Add(checkBoxFilterChampions, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(5, 5);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 395F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(1740, 440);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dataGridView
+            // 
+            dataGridView.BackgroundColor = Color.FromArgb(0, 0, 192);
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableLayoutPanel1.SetColumnSpan(dataGridView, 2);
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.Location = new Point(3, 48);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 62;
+            dataGridView.Size = new Size(1734, 389);
+            dataGridView.TabIndex = 1;
+            // 
+            // checkBoxFilterChampions
+            // 
+            checkBoxFilterChampions.Appearance = Appearance.Button;
+            checkBoxFilterChampions.BackColor = Color.FromArgb(0, 0, 192);
+            checkBoxFilterChampions.Dock = DockStyle.Fill;
+            checkBoxFilterChampions.ForeColor = Color.White;
+            checkBoxFilterChampions.Location = new Point(3, 3);
+            checkBoxFilterChampions.Name = "checkBoxFilterChampions";
+            checkBoxFilterChampions.Size = new Size(195, 39);
+            checkBoxFilterChampions.TabIndex = 2;
+            checkBoxFilterChampions.Text = "FilterChampions";
+            checkBoxFilterChampions.TextAlign = ContentAlignment.MiddleCenter;
+            checkBoxFilterChampions.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1750, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "MainForm";
+            Padding = new Padding(5);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Roster";
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dataGridView;
+        private CheckBox checkBoxFilterChampions;
+    }
+}
---
+++ filtered-data-grid-view/MainForm.cs
@@ -0,0 +1,197 @@
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace filtered_data_grid_view
+{
+    public partial class MainForm : Form
+    {
+        public MainForm() => InitializeComponent(); 
+        
+        string _filePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            Assembly.GetEntryAssembly().GetName().Name,
+            "roster.json"
+            );
+        BindingList<Wrestler> AllWrestlers { get; set; }
+        BindingList<Wrestler> FilteredWrestlers { get; } = new BindingList<Wrestler>();
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+#if false || REGEN
+
+            File.Delete( _filePath );
+#endif
+            if (!File.Exists(_filePath))
+            {
+                makeNewFile();
+            }
+            var json = File.ReadAllText(_filePath);
+            AllWrestlers = JsonConvert.DeserializeObject<BindingList<Wrestler>>(json);
+
+            // Here's the difference from the ListView answer
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.DataSource = FilteredWrestlers;
+            DataGridViewColumn columnToSwap = dataGridView.Columns[nameof(Wrestler.ShowTitles)];
+            int swapIndex =columnToSwap.Index;
+            var buttonColumn = new DataGridViewButtonColumn
+            {
+                Name = columnToSwap.Name,
+                HeaderText = columnToSwap.HeaderText,
+            };
+            buttonColumn.DefaultCellStyle.NullValue = "Show All";
+            // Style the buttons when painted.
+            dataGridView.CellPainting += (sender, e) =>
+            {
+                if ((e.ColumnIndex != -1) && (e.RowIndex != -1))
+                {
+                    if (dataGridView[e.ColumnIndex, e.RowIndex] is DataGridViewButtonCell buttonCell)
+                    {
+                        buttonCell.FlatStyle = FlatStyle.Flat;
+                        if (FilteredWrestlers[e.RowIndex].Titles.Any())
+                        {
+                            buttonCell.Style.BackColor = Color.FromArgb(0, 0, 192);
+                            buttonCell.Style.ForeColor = Color.White;
+                        }
+                        else
+                        {
+                            e.Handled = true;
+                            e.PaintBackground(e.ClipBounds, false);
+                        }
+                    }
+                }
+            };
+            // Show the titles when clicked
+            dataGridView.CellContentClick += (sender, e) =>
+            {
+                if ((e.RowIndex != -1) &&(dataGridView.Columns[nameof(Wrestler.ShowTitles)].Index == e.ColumnIndex))
+                {
+                    MessageBox.Show(string.Join(Environment.NewLine, FilteredWrestlers[e.RowIndex].Titles));
+                }
+            };
+            dataGridView.Columns.Remove(columnToSwap);
+            dataGridView.Columns.Insert(swapIndex, buttonColumn);
+            foreach (DataGridViewColumn col in dataGridView.Columns)
+            {
+                col.HeaderCell.Style.Font = new Font(dataGridView.Font.FontFamily, 8f);
+                switch (col.Name)
+                {
+                    case nameof(Wrestler.Name):
+                        col.ReadOnly = true;
+                        col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                        break;
+                    case nameof(Wrestler.Description):
+                        // Allow editing for Description
+                        col.ReadOnly = false;
+                        col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                        break;
+                    default:
+                        col.ReadOnly = true;
+                        col.Width = 100;
+                        break;
+                }
+            }
+            checkBoxFilterChampions.CheckedChanged += (sender, e) =>
+            {
+                ApplyFilter();
+            };
+            ApplyFilter();
+        }
+
+        private void ApplyFilter()
+        {
+            FilteredWrestlers.Clear();
+            if(checkBoxFilterChampions.Checked) 
+            {
+                foreach (var wrestler in AllWrestlers.Where(_=>_.Titles.Any()))
+                {
+                    FilteredWrestlers.Add(wrestler);
+                }
+            }
+            else
+            {
+                foreach (var wrestler in AllWrestlers)
+                {
+                    FilteredWrestlers.Add(wrestler);
+                }
+            }
+        }
+
+        private void makeNewFile()
+        {
+            var personsList = new List<Wrestler>    
+            {
+                new Wrestler
+                {
+                    Name = "The Rock",
+                    Description = "He is the most electrifying name in sports entertainment.",
+                    Titles = new string[]{ "WWE Heavyweight Champion (8x)", "WWF World Heavyweight Champion (3x)" },
+                    InRingAbility = 80,
+                    Charisma = 90,
+                    Gender = "Male",
+                    RingStyle = "Basic",
+                    DrawValue = 100,
+                    Condition = 80,
+                    Weight = 220,
+                    Morale = 100,
+                    MicSkills = 100,
+                },
+                new Wrestler
+                {
+                    Name = "MJF",
+                    Description = "First-ever World Middleweight Champion at the Battle Riot special.",
+                    Gender = "Male",
+                    Titles = new string[]{ "MLW World Middleweight Championship", "Rockstar Pro Tag Team Championship" }
+                },
+                new Wrestler
+                {
+                    Name = "David Flair",
+                    Gender = "Male",
+                },
+                new Wrestler
+                {
+                    Name = "Charlotte Flair",
+                    Gender = "Female",
+                },
+                new Wrestler
+                {
+                    Name = "Braden Putt",
+                    Description = "Rising star who everyone is watching.",
+                    Gender = "Male",
+                },
+            };
+            Directory.CreateDirectory(Path.GetDirectoryName(_filePath));
+            var json = JsonConvert.SerializeObject(personsList, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+            System.Diagnostics.Process.Start("notepad.exe", _filePath);
+        }
+    }
+    class Wrestler
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // This will be a button column. Cell will be visible if wrestler has > 0 titles.
+        public string? ShowTitles => null;
+        public string[] Titles { get; set; } = new string[0];
+
+        [Description("In Ring Ability")]
+        public int InRingAbility { get; set; }
+
+        [Description("Draw Value")]
+        public int DrawValue { get; set; }
+        public int Charisma { get; set; }
+        public int Condition { get; set; }
+        public int Weight { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public int Morale { get; set; }
+
+        [Description("Ring Style")]
+        public string RingStyle { get; set; } = string.Empty;
+
+        [Description("Mic Skills")]
+        public int MicSkills { get; set; }
+    }
+}
